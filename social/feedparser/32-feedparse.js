d49ac80f8111ae8539ac458ab0a8086b4d429fd6
Reza Alizadeh Majd <r.a.majd@gmail.com>
2015-10-11 21:25:26 +0330

    fix node-red carsh caused by feedparser with invalid url

--- social/feedparser/32-feedparse.js
+++ social/feedparser/32-feedparse.js
@@ -28,39 +28,46 @@ module.exports = function(RED) {
         this.seen = {};
         if (this.url !== "") {
             var getFeed = function() {
-                var req = request(node.url, {timeout: 10000, pool: false});
-                //req.setMaxListeners(50);
-                //req.setHeader('user-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36');
-                //req.setHeader('accept', 'text/html,application/xhtml+xml');
+                var req = null;
+                try {
+                    req = request(node.url, {timeout: 10000, pool: false});
+                    //req.setMaxListeners(50);
+                    //req.setHeader('user-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36');
+                    //req.setHeader('accept', 'text/html,application/xhtml+xml');
+                } catch (err) {
+                    node.error(err);
+                }
 
-                var feedparser = new FeedParser();
+                if (req != null) {
+                    var feedparser = new FeedParser();
 
-                req.on('error', function(err) { node.error(err); });
+                    req.on('error', function(err) { node.error(err); });
 
-                req.on('response', function(res) {
-                    if (res.statusCode != 200) { node.warn(RED._("feedparse.errors.badstatuscode")+" "+res.statusCode); }
-                    else { res.pipe(feedparser); }
-                });
+                    req.on('response', function(res) {
+                        if (res.statusCode != 200) { node.warn(RED._("feedparse.errors.badstatuscode")+" "+res.statusCode); }
+                        else { res.pipe(feedparser); }
+                    });
 
-                feedparser.on('error', function(error) { node.error(error); });
+                    feedparser.on('error', function(error) { node.error(error); });
 
-                feedparser.on('readable', function () {
-                    var stream = this, article;
-                    while (article = stream.read()) {  // jshint ignore:line
-                        if (!(article.guid in node.seen) || ( node.seen[article.guid] !== 0 && node.seen[article.guid] != article.date.getTime())) {
-                            node.seen[article.guid] = article.date?article.date.getTime():0;
-                            var msg = {
-                                topic: article.origlink || article.link,
-                                payload: article.description,
-                                article: article
-                            };
-                            node.send(msg);
+                    feedparser.on('readable', function () {
+                        var stream = this, article;
+                        while (article = stream.read()) {  // jshint ignore:line
+                            if (!(article.guid in node.seen) || ( node.seen[article.guid] !== 0 && node.seen[article.guid] != article.date.getTime())) {
+                                node.seen[article.guid] = article.date?article.date.getTime():0;
+                                var msg = {
+                                    topic: article.origlink || article.link,
+                                    payload: article.description,
+                                    article: article
+                                };
+                                node.send(msg);
+                            }
                         }
-                    }
-                });
+                    });
 
-                feedparser.on('meta', function (meta) {});
-                feedparser.on('end', function () {});
+                    feedparser.on('meta', function (meta) {});
+                    feedparser.on('end', function () {});
+                }
             };
             this.interval_id = setInterval(function() { getFeed(); }, node.interval);
             getFeed();
